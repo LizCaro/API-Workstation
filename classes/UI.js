1fb0f0a8ce09ecd51c6ff1c0c5b81b23443bd5ea
william herrera <wjha.1998@hotmail.com>
2021-01-19 14:47:11 -0500

    finish button

--- classes/UI.js
+++ classes/UI.js
@@ -12,27 +12,31 @@ export default class UI {
         const c_text =document.getElementById('c_text')
         const d_text =document.getElementById('d_text')
         const nextQuestions = document.getElementById('next-questions');
-        
+        const finishQuestions = document.getElementById('finish-questions');
         
 
         let quesitonsAll = []
+        let correct = []
 
         answers.forEach((element) => {
             const questionsCoices = element.question
             const answerChoices = [...element.incorrect_answers]
+            const corrected = element.correct_answer
             
             answerChoices.splice(Math.floor(Math.random() * 4) - 1, 0, element.correct_answer)
+
+            correct.push(corrected)
             quesitonsAll.push(questionsCoices)
             quesitonsAll.push(answerChoices)
 
         })
         
         for (let i = 0; i < quesitonsAll.length; i++) {
-                questionItem.innerText = quesitonsAll[0]
-                a_text.innerText = quesitonsAll[1][0]
-                b_text.innerText = quesitonsAll[1][1]
-                c_text.innerText = quesitonsAll[1][2]
-                d_text.innerText = quesitonsAll[1][3]   
+                questionItem.innerHTML = quesitonsAll[0]
+                a_text.innerHTML = quesitonsAll[1][0]
+                b_text.innerHTML = quesitonsAll[1][1]
+                c_text.innerHTML = quesitonsAll[1][2]
+                d_text.innerHTML = quesitonsAll[1][3]   
         } 
 
 
@@ -47,39 +51,41 @@ export default class UI {
             
             for (const rb of rbs) {
                 if(rb.checked) {
-                document.getElementById('a').setAttribute('value', a_text.innerText)
-                document.getElementById('b').setAttribute('value', b_text.innerText)
-                document.getElementById('c').setAttribute('value', c_text.innerText)
-                document.getElementById('d').setAttribute('value', d_text.innerText)
+                document.getElementById('a').setAttribute('value', a_text.innerHTML)
+                document.getElementById('b').setAttribute('value', b_text.innerHTML)
+                document.getElementById('c').setAttribute('value', c_text.innerHTML)
+                document.getElementById('d').setAttribute('value', d_text.innerHTML)
                 selecetValue = rb.value
                 selectedAnswers.push(selecetValue)
                 console.log(selectedAnswers)
-                // rb.checked = false;
+                rb.checked = false;
                 
                 }
             }
             const totalQuestions = document.getElementById('total-questions').value;
 
             if (contNumNext === totalQuestions - 1) {
-                nextQuestions.innerText = 'Finish'
+                nextQuestions.innerHTML = 'Finish';
+                // finishQuestions.style.display = 'block';
             }
 
-            questionItem.innerText = quesitonsAll[currentQuestion++ + 1];    
+            questionItem.innerHTML = quesitonsAll[currentQuestion++ + 1];    
             
-            if (a_text.innerText = quesitonsAll[currentQuestion+ 1]) {
-                a_text.innerText = quesitonsAll[currentQuestion+ 1][0]
-                b_text.innerText = quesitonsAll[currentQuestion+ 1][1]
-                c_text.innerText = quesitonsAll[currentQuestion+ 1][2]
-                d_text.innerText = quesitonsAll[currentQuestion+ 1][3]
+            if (a_text.innerHTML = quesitonsAll[currentQuestion+ 1]) {
+                a_text.innerHTML = quesitonsAll[currentQuestion+ 1][0]
+                b_text.innerHTML = quesitonsAll[currentQuestion+ 1][1]
+                c_text.innerHTML = quesitonsAll[currentQuestion+ 1][2]
+                d_text.innerHTML = quesitonsAll[currentQuestion+ 1][3]
 
                 contNumNext++
-            } else {
-                alert('finish')
-            }
+            } 
             
             
         })
         console.log(selectedAnswers)
         console.log(quesitonsAll)
+        finishQuestions.addEventListener('click', () => {
+
+        })
     }
 }
